3115e09d9304b6537a8caa8cd641c7fa204a42ca
Pierre Simon <pierre.simon.pro87@gmail.com>
2021-03-29 20:25:50 +0200

    [CONFIGURATION] Fix ios12 appDelegate configuration

--- LeboncoinTest/AppDelegate.swift
+++ LeboncoinTest/AppDelegate.swift
@@ -10,16 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
-
         let interactor = CategoryAndProductInteractor()
         let productsViewController = ProductsViewController(interactor: interactor)
         let rootNavigationController = UINavigationController(rootViewController: productsViewController)
         window.rootViewController = rootNavigationController
         window.makeKeyAndVisible()
-
+        self.window = window
         return true
     }
 
