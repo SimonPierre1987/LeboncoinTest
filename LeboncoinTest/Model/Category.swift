3e93db7397b08a37c95a90e7b25558608f014fd3
Pierre Simon <pierre.simon.pro87@gmail.com>
2021-03-29 10:04:02 +0200

    [MODEL] create Category model

---
+++ LeboncoinTest/Model/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Categories.swift
+//  LeboncoinTest
+//
+//  Created by Simon Pierre on 29/03/2021.
+//
+
+import Foundation
+
+struct Category: Decodable {
+    let id: Int
+    let name: String
+}
+
+struct Categories: Decodable {
+    let categories: [Category]
+}
