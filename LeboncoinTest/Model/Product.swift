0df3c240d1b9a4afcbfe85314aa2fd03e6e90bff
Pierre Simon <pierre.simon.pro87@gmail.com>
2021-03-29 10:15:52 +0200

    [MODEL] create a product model

---
+++ LeboncoinTest/Model/Product.swift
@@ -0,0 +1,25 @@
+//
+//  Product.swift
+//  LeboncoinTest
+//
+//  Created by Simon Pierre on 29/03/2021.
+//
+
+import Foundation
+
+struct Product: Decodable {
+    let id: Int
+    let category_id: Int
+    let title: String
+    let description: String
+    let price: Int
+    let images: ProductImages
+    let creation_date: String
+    let is_urgent: Bool
+    let siret: String?
+}
+
+struct ProductImages: Decodable {
+    let small: String
+    let thumb: String
+}
